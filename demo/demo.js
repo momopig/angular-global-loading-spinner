8a6bc67293c538c9afaeb7dc12b23898dbe37868
蔡剑涛 <caijiantao@haizhi.com>
2017-05-30 01:03:44 +0800

    1. Use the $httpProvider interceptor to add a timeout config to all http request; 2. Translate all the Chinese tips into English; 3. loadingConfig declares in $rootScope instead of directive $scope.

--- demo/demo.js
+++ demo/demo.js
@@ -1,12 +1,29 @@
 var demo = angular.module('demo', ['globalLoading']);
-demo.controller("DemoController", ['$scope', '$http', function ($scope, $http) {
-    $scope.loadingConfig = {
-        excludeApis: [{method:'get', url: '/api/site/entry'}],	// 站点列表页面,需要轮训该接口,所以排除该loading
-        timeout: 5000, //设置5秒的超时时间
-        timeoutCallback: function () {
-            console.error('请求超时');
-        }
-    };
+
+// it suggests that the loadingConifg declare in global code area
+demo.config(['$httpProvider', function($httpProvider) {
+
+    //inject $rootScope in interceptors, unable to inject in app's config function
+    $httpProvider.interceptors.push(function ($rootScope) {
+        $rootScope.loadingConfig = {
+            excludeApis: [{method: 'get', url: '/api/site/entry'}],	// 站点列表页面,需要5s轮询该接口以刷新列表, loading 样式也会5s显示一次，影响用户体验，所以排除该loading
+            timeout: 5000,
+            timeoutCallback: function () {
+                console.error('request timeout');
+            }
+        };
+        return {
+            'request': function(config) {
+                if ($rootScope.loadingConfig.timeout !== undefined) {
+                    config.timeout = $rootScope.loadingConfig.timeout;
+                }
+                return config;
+            }
+        };
+    });
+}]);
+demo.controller("DemoController", ['$scope', '$http', '$rootScope', function ($scope, $http) {
+
     $scope.getFile = function() {
          $http({
             method: 'get',
--- src/core/index.js
+++ src/core/index.js
@@ -3,14 +3,11 @@ var style = require('./index.css');
 
 module.exports = function(mod) {
 
-	mod.directive('globalLoading', ['$window', '$http', '$timeout', function($window, $http, $timeout) {
+	mod.directive('globalLoading', ['$http', '$timeout', '$rootScope', function($http, $timeout, $rootScope) {
 		return {
 			restrict: 'EA',
 			replace: true,
 			transclde: true,
-			scope:{
-				loadingConfig: '='
-			},
 			template: tpl,
 			link: function (scope, elm, attrs) {
 
@@ -21,8 +18,8 @@ module.exports = function(mod) {
 					pendingLength = $http.pendingRequests.length;
 
 					$http.pendingRequests.forEach(function(item){
-						for(i = 0, l = scope.loadingConfig.excludeApis.length; i < l; i++) {
-							apiObj = scope.loadingConfig.excludeApis[i];
+						for(i = 0, l = $rootScope.loadingConfig.excludeApis.length; i < l; i++) {
+							apiObj = $rootScope.loadingConfig.excludeApis[i];
 							if(item.url === apiObj.url && item.method === apiObj.method.toUpperCase()) {
 								pendingLength--;
 							}
@@ -31,27 +28,22 @@ module.exports = function(mod) {
 					return pendingLength > 0;
 				};
 
+
+
 				/*
 				*  isLoading fn  gets called multiple times per digest,
 				* the watch callback will be called when the isLoading fn's return value changes.
 				*
 				* */
-				scope.$watch(scope.isLoading, function (v)
-				{
-                    if(typeof scope.loadingConfig.timeout === 'number') {
-                        $timeout(function() {
-                            var pendingLength = $http.pendingRequests.length;
-                            if (pendingLength > 0) {
-							$(elm).hide();
-                                $http.pendingRequests = [];
-                                if (typeof scope.loadingConfig.timeoutCallback === 'function') {
-                                    scope.loadingConfig.timeoutCallback();
-								}
-                            }
-                        }, scope.loadingConfig.timeout);
-                    }
+				scope.$watch(scope.isLoading, function (v) {
 					if(v){
 						$(elm).show();
+                        if (typeof $rootScope.loadingConfig.timeout === 'number' &&
+                            typeof $rootScope.loadingConfig.timeoutCallback === 'function') {
+                            $timeout(function() {
+                                    $rootScope.loadingConfig.timeoutCallback();
+                            }, $rootScope.loadingConfig.timeout);
+                        }
 					}else{
                         $(elm).hide();
 					}
